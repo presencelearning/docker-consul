2c666a03e78b3d24086fe8236319c2663bc3241e
Francois Gaudin <contact@francoisgaudin.com>
2015-03-03 13:10:52 -0800

    Fixed dockerfile

--- start.sh
+++ start.sh
@@ -5,4 +5,4 @@ if [ -n "$SERVERS" ]; then
     PARAM="-server -bootstrap-expect $SERVERS -ui-dir /opt/consul_ui/dist -client 0.0.0.0";
 fi
 
-consul agent -data-dir /tmp/consul $PARAM
+consul agent -data-dir /tmp/consul $PARAM $@
